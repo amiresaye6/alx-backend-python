bd4f2ec2a574095d5a6aee6fbc4ce1dd7c1430cc
Amir Alsayed <amyralsyd367@gmail.com>
2024-04-18 20:38:24 +0200

    changing directories

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+task: 1. Basic annotations - concat
+condition: mandatory
+required: Write a type-annotated function concat that takes a string str1 and
+a string str2 as arguments and returns a concatenated string
+"""
+
+
