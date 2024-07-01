f11868d095c2fa2617da0fbf06f0c78fe1406f23
Amir Alsayed <amyralsyd367@gmail.com>
2024-07-01 18:42:32 +0300

    second test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
 0. Parameterize a unit test
+1. Parameterize a unit test
 """
 import unittest
 from utils import access_nested_map
@@ -32,4 +33,21 @@ class TestAccessNestedMap (unittest.TestCase):
         Returns:
             None
         """
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+        response = access_nested_map(nested_map, path)
+        self.assertEqual(response, expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map: Mapping,
+                                         path: Sequence) -> None:
+        """
+        Test the access_nested_map method raises an error when expected to
+        Args:
+            nested_map (Dict): A dictionary that may have nested dictionaries
+            path (List, tuple, set): Keys to get to the required value in the
+                                     nested dictionary
+        """
+        with self.assertRaises(Exception):
+            access_nested_map(nested_map, path)
